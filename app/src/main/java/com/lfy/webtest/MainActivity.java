6bc7e77aafb95892492248a9802479914c9c8d45
liufuyi <109335992@qq.com>
2019-08-09 14:47:43 +0800

    init

---
+++ app/src/main/java/com/lfy/webtest/MainActivity.java
@@ -0,0 +1,92 @@
+package com.lfy.webtest;
+
+import android.app.Activity;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web_view);
+
+        init();
+    }
+
+    private void init(){
+        WebSettings set = webView.getSettings();
+        set.setJavaScriptEnabled(true);
+        set.setUseWideViewPort(true);
+        set.setLoadWithOverviewMode(true);
+
+        set.setAllowFileAccess(false);
+        set.setAllowFileAccessFromFileURLs(false);
+        set.setAllowUniversalAccessFromFileURLs(false);
+
+        set.setLoadsImagesAutomatically(true);
+        set.setBlockNetworkImage(false);//解决图片不显示
+        //set.setJavaScriptCanOpenWindowsAutomatically(true);
+
+
+        set.setDefaultTextEncodingName("UTF-8");
+        set.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        set.setDomStorageEnabled(true);
+        set.setDatabaseEnabled(false);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            set.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        webView.setWebViewClient(new MyWebViewClient());
+
+        webView.loadUrl("https://iq.uniqlo.cn/");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (webView != null) {
+            webView.onPause();
+            webView.pauseTimers();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (webView != null) {
+            webView.resumeTimers();
+            webView.onResume();
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
+    }
+
+    private class MyWebViewClient extends WebViewClient{
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
+            handler.proceed();
+        }
+    }
+
+}
